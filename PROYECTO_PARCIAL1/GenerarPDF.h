edde0ff1f4cba351d8400907ab2713240d0d11c9
Maycol Stiven Celi Gómez <141872982+xMaycol08@users.noreply.github.com>
2024-12-05 17:59:37 -0500

    Delete PROYECTO_PARCIAL1 directory

--- PROYECTO_PARCIAL1/GenerarPDF.h
+++
@@ -1,9 +0,0 @@
-#ifndef GENERARPDF_H
-#define GENERARPDF_H
-
-#include "ListaCircularDoble.h"
-
-// Función para generar el PDF con los datos de los libros
-void generarPDF(const ListaCircularDoble& lista);
-void generarPDFPorISBN(const ListaCircularDoble& lista);
-#endif
