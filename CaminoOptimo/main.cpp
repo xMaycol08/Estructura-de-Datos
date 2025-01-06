9eb5e8a2c86db0c79e2957e85e0592f506c1585f
Maycol Stiven Celi Gómez <141872982+xMaycol08@users.noreply.github.com>
2025-01-05 22:14:20 -0500

    Update main.cpp

--- CaminoOptimo/main.cpp
+++ CaminoOptimo/main.cpp
@@ -1,3 +1,10 @@
+/***********************************************************************
+ * UNIVERSIDAD DE LAS FUERZAS ARMADAS "ESPE"
+ * ALUMNOS:  Maycol Celi
+ * FECHA ENTREGA:  06 de enero de 2025
+ * PROGRAMA: Busqueda Binaria - Camino Optimo.
+ * NRC: 1992
+ ***********************************************************************/
 #include <iostream>
 #include <vector>
 #include <limits> // Incluir este encabezado para std::numeric_limits
